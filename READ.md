8a7bb9354179f1460d6f1c498ba1e0e0259aa79e
emperez <emperez@bu.edu>
2022-09-02 14:59:34 -0500

    first commit

---
+++ READ.md
@@ -0,0 +1,1 @@
+"# Front Page Of Cars" 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+const appRoutes: Routes = [
+  {path: ''}
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes,{enableTracing: false}),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
